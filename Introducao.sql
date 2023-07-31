0237358f51cf0f463ce2720a2b2ec403249a2f15
EstagiarioComigo <140831709+EstagiarioComigo@users.noreply.github.com>
2023-07-31 09:59:27 -0300

    Adicionando arquivo

---
+++ Introducao.sql
@@ -0,0 +1,12 @@
+
+Select SUM(lancamentos."Lancamentos"."Valor")as SomaDasDespesas from lancamentos."Lancamentos" where lancamentos."Lancamentos"."TipoDoLancamento_Id"='despesa';
+
+Select * from lancamentos."Lancamentos";
+
+-- Primeiro registro no banco de dados(usando o created at)
+Select * from lancamentos."Lancamentos" order by created_at asc limit 1;
+select * from lancamentos."Lancamentos" order by "created_at" asc fetch first 10 rows only;
+
+-- Lançamento mais antigo no banco de dados(o correto é usar o campo vencimento)
+Select * from lancamentos."Lancamentos";
+select * from lancamentos."Lancamentos" order by "Vencimento" asc fetch first 5 rows only;
