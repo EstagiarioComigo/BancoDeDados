7e39532fb484f18f40473e673dc786c7f7e4cd2d
EstagiarioComigo <140831709+EstagiarioComigo@users.noreply.github.com>
2023-07-31 13:03:45 +0000

    Left join com 3 tabelas

--- Introducao.sql
+++ Introducao.sql
@@ -10,3 +10,13 @@ select * from lancamentos."Lancamentos" order by "created_at" asc fetch first 10
 -- Lançamento mais antigo no banco de dados(o correto é usar o campo vencimento)
 Select * from lancamentos."Lancamentos";
 select * from lancamentos."Lancamentos" order by "Vencimento" asc fetch first 5 rows only;
+
+
+-- left join com 3 tabelas
+select "Lancamentos"."Descricao",CCI."Nome","CentroDeCustos"."Nome","Lancamentos"."Valor","Movimentacoes"."ValorPago",public."Movimentacoes"."DataDePagamento" from lancamentos."Lancamentos"
+    left join public."CentroDeCustos_Itens" as CCI
+        on "Lancamentos"."CentroDeCustoItem_Id" = CCI."Id"
+    left join public."CentroDeCustos"
+        on CCI."CentroDeCustos_Id" = public."CentroDeCustos"."Id"
+    left join public."Movimentacoes"
+        on public."Movimentacoes"."Lancamento_Id"="Lancamentos"."Id";
