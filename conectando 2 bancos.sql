65482a86504f149af87c6f64aa74578cc91361df
EstagiarioComigo <140831709+EstagiarioComigo@users.noreply.github.com>
2023-08-01 07:50:05 -0300

    inicial

--- conectando 2 bancos.sql
+++ conectando 2 bancos.sql
@@ -1,3 +1,5 @@
-select * from lancamentos."Lancamentos";
-
-select * from idendadades."usuarios";
+SELECT "Cliente_Id", CL.Nome FROM financeiro.lancamentos."Lancamentos" LC
+        LEFT JOIN
+        dblink('dbname=identidades','SELECT CLIENTES."Id", "Nome" FROM identidades.pessoas."Clientes" AS CLIENTES LEFT JOIN identidades.public."Pessoas" ON Clientes."Pessoa_Id" = "Pessoas"."Id"')
+            AS CL ("Id" uuid, Nome text)
+            ON LC."Cliente_Id" = CL."Id";
