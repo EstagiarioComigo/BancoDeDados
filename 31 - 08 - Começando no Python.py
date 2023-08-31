40ee18200ca97916b05bd0667915acfa9a7a083d
Leydyson - Estagiario Comigo <140831709+EstagiarioComigo@users.noreply.github.com>
2023-08-31 09:39:01 -0300

    inicial

---
+++ 31 - 08 - Começando no Python.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+conexao = psycopg2.connect(database = 'financeiro',
+                           host='18.215.83.195',
+                           user = "postgres",
+                           password = "postgrespassword",
+                           port = "5401")
+
+print(conexao.info)
+print(conexao.status)
+
+cursor = conexao.cursor()
+cursor.execute('SELECT * FROM financeiro.lancamentos."Lancamentos" as LC;')
+resultado = cursor.fetchall()
+
+for linha in resultado:
+    lancamento_id = linha[0]  # Suponha que o primeiro campo seja o ID do lançamento
+    consulta_sql = f"""
+        SELECT *
+        FROM financeiro.public."Movimentacoes" AS MV
+        WHERE MV."Lancamento_Id" = '{lancamento_id}'
+    """
+    cursor.execute(consulta_sql)
+    movimentacoes = cursor.fetchall()
+
+    if movimentacoes:  # Verifica se há movimentações para o lançamento
+        print(f"Movimentações do lançamento {lancamento_id}:")
+        for movimentacao in movimentacoes:
+            print(movimentacao)
+    else:
+        print(f"Nenhuma movimentação para o lançamento {lancamento_id}")
+
+cursor.close()
+conexao.close()
